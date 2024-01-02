c2b1b455ca6c06ce20801ccc753c7394a46a9723
David-konate <56818495+David-konate@users.noreply.github.com>
2024-01-02 13:41:30 +0100

    authentification en place

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -7,5 +7,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class Role extends Model
 {
-    use HasFactory;
+
+
+    //pluriel car un role peut avoirplusieurs users
+    //cardinalité 1,n
+    public function users(){
+        return $this->hasMany(User::class);
+    }
+
+    //nom au pluriel car un user peut poster plusieurs posts
+    // 0,n
+    public function posts()
+    {
+        return $this->hasMany(Comment::class);
+    }
+
+    //nom au singulier car 1 seul role
+    public function role()
+    {
+        return $this->belongsTo(Role::class);
+    }
+
+    public function isAdmin()
+    {
+        return $this->role_id == 2;
+    }
 }
